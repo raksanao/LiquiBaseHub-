11d0b824413a11840cc70c042f785747073eb0ab
Rukhshona Olimova <rukhshona87@gmail.com>
2020-11-04 12:46:08 -0600

    new commit

---
+++ src/test/java/com/automation/tests/TestCase2.java
@@ -0,0 +1,28 @@
+package com.automation.tests;
+
+import com.automation.pages.LoginPage;
+import com.automation.pages.ProjectPage;
+import com.automation.utilities.BrowserUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCase2 extends AbstractTestBase {
+    @Test
+    public void login() {
+        LoginPage login = new LoginPage();
+        login.login();
+        BrowserUtils.wait(4);
+        login.closePopUp();
+        ProjectPage project = new ProjectPage();
+        project.cliktoProjects();
+        project.expandModule();
+        project.clickToOperationTab();
+        BrowserUtils.wait(3);
+        project.clickToResult();
+
+        project.clickPass();
+        BrowserUtils.wait(2);
+        project.clickDropDownOperat();
+
+    }
+}
